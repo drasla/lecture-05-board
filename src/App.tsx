5e51049c06fe98452bdb1ad626ba11a246aa2868
Tae.Garam (Office) <drasla217@gmail.com>
2026-04-15 13:20:50 +0900

    Detail 완성

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<Home />} />
                 <Route path={"/:id"} element={<Detail />} />
+                <Route path={"/abc"} element={<Detail />} />
             </Routes>
         </BrowserRouter>
     );
